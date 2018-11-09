2990c8fb5e4385fa58ec699a32854fdd448ad008
Saad Malik <saad.t.malik@gmail.com>
2018-11-09 19:22:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,15 @@
-def countdown(number)
-  while number > 0
-    number -= 1
-    puts "#{number} SECOND(S)!"
+# def countdown(number)
+#   while number > 0
+#     number -= 1
+#     puts "#{number} SECOND(S)!"
+#   end
+#   puts "HAPPY NEW YEAR!"
+# end
+
+def countdown(count)
+  while count >0
+    puts "#{count} seconds!"
+    count-=1
   end
-  puts "HAPPY NEW YEAR!"
+  "0 left. HAPPY NEW YEAR!"
 end
